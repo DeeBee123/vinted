5b63fac626830b59a87600d5762a3223d10b8d07
DeeBee123 <barauskaite.deimante@gmail.com>
2021-04-29 15:37:59 +0300

    adding style to nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 // import {Layout} from "./components/layout/Layout"
 import {News} from "./features/news/News"
 import {Header} from "./components/header/Header"
+import {Nav} from "./components/nav/Nav"
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Header/>
       <News />
     </div>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,51 +1,44 @@
 import React from "react";
 import "./nav.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSortDown, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export const Nav = () => {
   return (
     <nav className="navigation">
       <div className="navigation__section">
         <span className="logo">Vinted</span>
-
-        <form action="" className="search-form">
-          <select name="" id="" className="form-selection">
-            <option value="goods" selected className="form-selection__option">
+        <div className="right">
+          <div className="goods-div">
+            <button>
               Prekės
-            </option>
-            <option value="members" className="form-selection__option">
-              Nariai
-            </option>
-            <option value="forum" className="form-selection__option">
-              Forumas
-            </option>
-            <option value="help" className="form-selection__option">
-              Pagalbos centras
-            </option>
-          </select>
+              <FontAwesomeIcon className="arrow" icon={faSortDown} />
+            </button>
 
-          <div className="search">
-            <img src="" alt="" className="search__icon" />
-            <input
-              type="search"
-              placeholder="Ieškoti prekių"
-              className="search__input"
-            />
+            <ul className="drop-down">
+              <li>Prekės</li>
+              <li>Nariai</li>
+              <li>Forumas</li>
+              <li>Pagalbos centras</li>
+            </ul>
           </div>
-        </form>
+          <form action="" className="search-form">
+              <FontAwesomeIcon className="search-icon" icon={faSearch} />
+              <input
+                type="search"
+                placeholder="Ieškoti prekių"
+                className="input"
+              />
+          </form>
 
-        <button className="register-sign-btn">
-          Registruotis <span>|</span> Prisijungti
-        </button>
-        <button className="sell-btn">Parduok dabar</button>
-        <div>
-          <span>?</span>
+          <button className="btn-global register-btn">
+            Registruotis <span>|</span> Prisijungti
+          </button>
+          <button className="btn-global btn-global-primary custom-sign">Parduok dabar</button>
+   
+           <button className="question">?</button>
+         
         </div>
-        <select name="" id="" className="lang-selection">
-          <option value="" selected>
-            LT
-          </option>
-          <option value="">Lietuvių (Lithuanian)</option>
-        </select>
       </div>
     </nav>
   );
