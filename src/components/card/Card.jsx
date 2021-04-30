a179c183d25c6db07a9f80ed100371f46263c7c6
DeeBee123 <barauskaite.deimante@gmail.com>
2021-04-30 13:23:46 +0300

    adding filter functionality

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -10,7 +10,6 @@ const Card = ({ productID, brands, input }) => {
   useEffect(() => {
     API.get(`products/${productID}`)
       .then((response) => {
-        console.log(response.data)
         setData(response.data);
       })
       .catch((error) => console.log(error));
@@ -24,15 +23,37 @@ const Card = ({ productID, brands, input }) => {
       })
       .catch((error) => console.log(error));
   }, [data.user]);
-const handleFilter = ()=> {
-  let copyData = [...data].filter(data=> data.title.includes(input))
-  input =""
-  setData(copyData)
-}
-  return (
-    <div className="card">
-      {data.length !== 0 && input==="" ? (
-        <>
+
+  return (  <>
+   
+      {data.length !== 0 && input===""&& (
+       <div className="card">  <div className="user-row">
+       <div
+         className="avatar"
+         style={user.avatar && { backgroundImage: `url(${user.avatar})` }}
+       ></div>
+       <span className="user-name">{user.name}</span>
+     </div>
+     <div
+       className="image"
+       style={{ backgroundImage: `url(${data.img[data.main_img]})` }}
+     ></div>
+     <div className="info">
+       <div className="row">
+         <span className="price">{data.price.toFixed(2)} &euro;</span>
+       </div>
+       {data.size.length > 0 && (
+         <span className="size">{data.size.join("/")}</span>
+       )}
+       <div className="brand">
+         {brands.map((brand) => brand.id === data.brand && brand.title)}
+       </div>
+     </div></div>
+        
+       
+      )}
+      {input.length >0 && data.title.includes(input)&& (
+       <div className="card">
           <div className="user-row">
             <div
               className="avatar"
@@ -55,10 +76,9 @@ const handleFilter = ()=> {
               {brands.map((brand) => brand.id === data.brand && brand.title)}
             </div>
           </div>
-        </>
-      ): handleFilter}
-      
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
