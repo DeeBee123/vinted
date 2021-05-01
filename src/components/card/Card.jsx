95dea0b9e27ae2b48ae6a65385823c0bd3bebc75
DeeBee123 <barauskaite.deimante@gmail.com>
2021-05-01 15:36:35 +0300

    adding lowercase to filters and data.title

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -16,44 +16,19 @@ const Card = ({ productID, brands, input }) => {
   }, [productID]);
 
   useEffect(() => {
-    data.user &&  API.get(`users/${data.user}`)
-      .then((response) => {
-      
-       setUser(response.data);
-      })
-      .catch((error) => console.log(error));
+    data.user &&
+      API.get(`users/${data.user}`)
+        .then((response) => {
+          setUser(response.data);
+        })
+        .catch((error) => console.log(error));
   }, [data.user]);
 
-  return (  <>
-   
-      {data.length !== 0 && input===""&& (
-       <div className="card">  <div className="user-row">
-       <div
-         className="avatar"
-         style={user.avatar && { backgroundImage: `url(${user.avatar})` }}
-       ></div>
-       <span className="user-name">{user.name}</span>
-     </div>
-     <div
-       className="image"
-       style={{ backgroundImage: `url(${data.img[data.main_img]})` }}
-     ></div>
-     <div className="info">
-       <div className="row">
-         <span className="price">{data.price.toFixed(2)} &euro;</span>
-       </div>
-       {data.size.length > 0 && (
-         <span className="size">{data.size.join("/")}</span>
-       )}
-       <div className="brand">
-         {brands.map((brand) => brand.id === data.brand && brand.title)}
-       </div>
-     </div></div>
-        
-       
-      )}
-      {input.length >0 && data.title.includes(input)&& (
-       <div className="card">
+  return (
+    <>
+      {data.length !== 0 && input === "" && (
+        <div className="card">
+          {" "}
           <div className="user-row">
             <div
               className="avatar"
@@ -78,6 +53,35 @@ const Card = ({ productID, brands, input }) => {
           </div>
         </div>
       )}
+      {input.length > 0 &&
+        data.title.toLowerCase().includes(input.toLowerCase())&&(
+          <div className="card">
+            <div className="user-row">
+              <div
+                className="avatar"
+                style={
+                  user.avatar && { backgroundImage: `url(${user.avatar})` }
+                }
+              ></div>
+              <span className="user-name">{user.name}</span>
+            </div>
+            <div
+              className="image"
+              style={{ backgroundImage: `url(${data.img[data.main_img]})` }}
+            ></div>
+            <div className="info">
+              <div className="row">
+                <span className="price">{data.price.toFixed(2)} &euro;</span>
+              </div>
+              {data.size.length > 0 && (
+                <span className="size">{data.size.join("/")}</span>
+              )}
+              <div className="brand">
+                {brands.map((brand) => brand.id === data.brand && brand.title)}
+              </div>
+            </div>
+          </div>
+        )}
     </>
   );
 };
